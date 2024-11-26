904970cc83c8680f90917a47d88252415848f8a1
Leyla Kutlu <kutlul1@mcmaster.ca>
2024-11-26 17:28:10 -0500

    modified credit and debit files

---
+++ credit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "credit.h"
+
+//store credit balance
+
+//function to spend money with credit card
+void spend_money() {
+    
+}
+
+//function to check balance for credit card
+void check_balance() {
+
+}
---
+++ debit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "debit.h"
+#include "credit.h"
+
+//store debit balance
+
+//function to add money to debit card
+void add_to_debit() {
+    if 
+    //return debit balance
+}
+
+//function to spend money with debit card
+void spend_money() {
+    
+}
+
+//function to pay off credit with debit card
+void pay_off_credit() {
+    
+}
+
+//function to check balance for debit card
+void check_balance() {
+
+}
+
