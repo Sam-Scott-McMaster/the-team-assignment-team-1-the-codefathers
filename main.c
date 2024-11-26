6bf0c02f7e415585d9ce8d62c8dd79860c8fe41a
rjema <79913700+rjema@users.noreply.github.com>
2024-11-26 17:21:26 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include "user_account.h"
 #include "new_account.h"
 int main() { 
-    char username[50]; 
+    //char username[50]; 
 
-    printf("Enter your username: ");
-    scanf("%49s", username);
+    //printf("Enter your username: ");
+    //scanf("%49s", username);
 
-    create_user_history_file(username);
-
+    //create_user_history_file(username);
+    char *username = scan_username();
+    char *phone_num = scan_phone();
+    char *birthday = scan_birthday();
     return 0;
 }
