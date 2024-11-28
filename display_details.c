47230012778185f6d8518912db98bf9b26913c0b
Fatima Alsalahi <alsalahf@mcmaster.ca>
2024-11-28 01:29:02 -0500

    modified display_details.c

--- display_details.c
+++ display_details.c
@@ -4,32 +4,6 @@
 #include "display_details.h"
 #include "budget.h"
 
-//helper function to read user data from a file
-int load_user_info(const char *username, char *account_type, double *debit_balance, double *credit_balance, double *balance, char *name) {
-    char filename[100]; //char array to store the filename
-    sprintf(filename, "%s.txt", username); //constructs the file name by appending ".txt" to the username and stores it in filename
-
-    //opens file with constructed filename in read mode. Returns a pointer to the file if successful
-    FILE *file = fopen(filename, "r");
-
-    if (!file) {
-        //checks if file pointer is NULL
-        printf("Error: Could not find user data for '%s'.\n", username);
-        return 0;
-    }
-
-    fscanf(file, "Account Type: %s\n", account_type); //Reads account type from the file and stores it
-    fscanf(file, "Debit Balance: %lf\n", debit_balance); //Reads debit balance
-    //gonna change acoridng to if value is string
-    fscanf(file, "Credit Balance: %lf\n", credit_balance); //Reads credit balance
-
-    fgets(name, 100, file); //Read name from the file using fgets, removing the newline char at the end
-    name[strcspn(name, "\n")] = '\0'; //Removes the newline char if it's there
-
-    fclose(file); //closes file
-    return 1; //successful
-}
-
 void display_account_type(const char *account_type) {
 
     if (strcmp(account_type, "Both") == 0) { //compares account type
@@ -39,8 +13,14 @@ void display_account_type(const char *account_type) {
 
 void display_account_balance(const char *account_type, double debit_balance, double credit_balance) {
 
-        printf("Debit Account Balance: $%.2f\n", debit_balance);
-        printf("Credit Account Balance: $%.2f\n", credit_balance);
+    double debit_balance = 0.0, credit_balance = 0.0;
+
+    get_recent_debit_balance("transaction_logs", username, &debit_balance);
+    get_recent_credit_balance("transaction_logs", username, &credit_balance);
+
+    // Print the balances
+    printf("Your current debit balance: $%.2f\n", debit_balance);
+    printf("Your current credit balance: $%.2f\n", credit_balance);
 }
 
 void display_personal_info(const char *name) {
