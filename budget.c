a06002165db0db05f224ae0837601c5e2d070533
nkaruna09 <nithika.karuna@gmail.com>
2024-11-28 01:16:05 -0500

    update budget.c

--- budget.c
+++ budget.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 #include "user_account.h"
 
-void setBudget(const char *username, double budget){ 
-    void set_budget_to_user_file(const char *folder_name, username, budget);
-}
-
 double returnBudget(const char *username){ 
     return get_budget_from_user_file(username); 
 }
