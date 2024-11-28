1566b3bed82fecad96ea2593c3eab479ab4cadf9
Fatima Alsalahi <alsalahf@mcmaster.ca>
2024-11-28 00:08:13 -0500

    modified login.c

--- login.c
+++ login.c
@@ -49,6 +49,7 @@ int check_credentials(const char *username, char *stored_username, char *stored_
     fclose(file);
     return 1;
 }
+}
 
 //log in the user (takes username and password as input)
 int login(const char *username, const char *password) {
