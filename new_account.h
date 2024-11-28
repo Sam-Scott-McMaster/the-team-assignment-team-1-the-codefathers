536bb33dbeb067a538b1750591ab60f5617a0851
stellaxyl <stellaxyliu05@gmail.com>
2024-11-27 23:03:37 -0500

    email

--- new_account.h
+++ new_account.h
@@ -2,7 +2,7 @@ char *scan_birthday();
 char *scan_username();
 char *check_first_name();
 char *check_last_name();
-void check_email();
+char *check_email();
 char *scan_phone();
 char *password_hashing(char *, char*);
 char *password_processing(char *);
